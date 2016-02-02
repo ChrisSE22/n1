4c9ff4c23c72e2187831315ecc8f85c879e7febd
C Merry <cmerry@cs-imac.lan>
2016-02-02 11:31:36 +0000

    adding jpeg file type to html server

--- html_server.js
+++ html_server.js
@@ -12,7 +12,8 @@ http.createServer(function(request, response) {
   var contentTypesByExtension = {
     '.html': "text/html",
     '.css':  "text/css",
-    '.js':   "text/javascript"
+    '.js':   "text/javascript",
+	'.jpeg': 'image/jpeg'
   };
 
   fs.exists(filename, function(exists) {
